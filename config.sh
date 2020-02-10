d589149bb1bc7f1b0479cc3b232c7ca6bf5c7175
Marwan Haroun <ms.haroun@hotmail.com>
2020-02-10 16:05:25 -0500

    Updating config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,7 @@
 # INSTALL PYTHON3 LIBRARIES:
 sudo apt-get install python3-setuptools python-dev python3-pip vim tree
 sudo pip3 install adafruit-circuitpython-mcp3xxx Adafruit_MCP3008 HX711
+
+# CREATING LINKS:
+sudo ln -s /home/pi/vacuum_meter/bin/start /bin/measure
+sudo ln -s /home/pi/vacuum_meter/dev_script/pull.sh /bin/pull
