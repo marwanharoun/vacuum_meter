4ad22254815616f816c4af6293af7ba8ebd35b03
Marwan Haroun <ms.haroun@hotmail.com>
2020-02-10 16:07:24 -0500

    Removing bak folder

--- bak/200202-1932/config.sh
+++
@@ -1,4 +0,0 @@
-# INSTALL PYTHON3 LIBRARIES:
-sudo pip3 install adafruit-circuitpython-mcp3xxx
-sudo pip3 install Adafruit_MCP3008
-sudo pip3 install HX711
--- bak/200206-1528/config.sh
+++
@@ -1,3 +0,0 @@
-# INSTALL PYTHON3 LIBRARIES:
-sudo apt-get install python3-setuptools python-dev python3-pip vim tree
-sudo pip3 install adafruit-circuitpython-mcp3xxx Adafruit_MCP3008 HX711
