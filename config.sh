084832bdf871cff5b495114e9ef5b697160363d9
Marwan Haroun <ms.haroun@hotmail.com>
2020-02-13 16:20:11 -0500

    Added script for vac only

--- config.sh
+++ config.sh
@@ -3,5 +3,7 @@ sudo apt-get install python3-setuptools python-dev python3-pip vim tree
 sudo pip3 install adafruit-circuitpython-mcp3xxx Adafruit_MCP3008 HX711
 
 # CREATING LINKS:
+
 sudo ln -s /home/pi/vacuum_meter/bin/start /bin/measure
+sudo ln -s /home/pi/vacuum_meter/bin/start_vac_only /bin/vac
 sudo ln -s /home/pi/vacuum_meter/dev_script/pull.sh /bin/pull
