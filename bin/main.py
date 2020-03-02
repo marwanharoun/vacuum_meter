2d66d0ee87c86c80e09532701094a0a200bfcf44
Marwan Haroun <ms.haroun@hotmail.com>
2020-03-02 18:37:21 -0500

    Increased sleep time

--- bin/main.py
+++ bin/main.py
@@ -39,7 +39,7 @@ def get_values():
             print("{0:.2f}".format(pressure),"  ","{0:.2f}".format(load),"{0:.2f}".format(flow_rate))
             row = {'timestamp': '{:%Y-%m-%d %H:%M:%S:%f}'.format(datetime.datetime.now()), 'load':load, 'pressure':pressure, 'flow':flow_rate}
             rows.append(row)
-            time.sleep(0.25)
+            time.sleep(0.5)
     except KeyboardInterrupt:
         print(rows)
         create_csv(rows)
