bebd887bb496704253d5032c93abe0bc88e56f3b
Marwan Haroun <ms.haroun@hotmail.com>
2020-03-02 18:02:29 -0500

    Cleaned scripts after flow.py

--- bin/flow.py
+++ bin/flow.py
@@ -6,11 +6,12 @@ import time
 class Flow:
 
     def __init__(self):
+        CHANNEL = 27
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(27, GPIO.IN)
+        GPIO.setup(CHANNEL, GPIO.IN)
         global rev_count
         rev_count = 0
-        GPIO.add_event_detect(27, GPIO.RISING, callback=self.increase_rev)
+        GPIO.add_event_detect(CHANNEL, GPIO.RISING, callback=self.increase_rev)
 
     def increase_rev(self,channel):
         global rev_count
@@ -19,13 +20,6 @@ class Flow:
     
     def get_value(self):
         global rev_count
-        time.sleep(0.25)
-        print("We are at ", rev_count)
         value = rev_count
         rev_count = 0
         return value
-
-
-#flow = Flow()
-#while True:
-#    flow.get_value()
