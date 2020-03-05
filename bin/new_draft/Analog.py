853d5c2f12c5391729cc092303e0176fcb2a23bb
Marwan Haroun <ms.haroun@hotmail.com>
2020-03-05 13:24:44 -0500

    Adjusting stuff

--- bin/new_draft/Analog.py
+++ bin/new_draft/Analog.py
@@ -10,7 +10,7 @@ class AnalogInput:
     def __init__(self):
         SPI_PORT   = 0
         SPI_DEVICE = 0
-        CHANNEL = 0
+        self.CHANNEL = 0
         self.mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
         #global analog_read
         #analog_read = self.return_calib(self.mcp.read_adc(CHANNEL))
